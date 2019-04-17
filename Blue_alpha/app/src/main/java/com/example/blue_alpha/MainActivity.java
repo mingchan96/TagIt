cd19bb013961519f8427541ac25a285a3d0ae981
mingchan96 <davc@ude.edu>
2019-04-17 18:23:53 -0400

    Change arrow image, added in a method to add anchor, a method to give a rough direction where to face

--- Blue_alpha/app/src/main/java/com/example/blue_alpha/MainActivity.java
+++ Blue_alpha/app/src/main/java/com/example/blue_alpha/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.blue_alpha;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,10 +17,13 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.ar.core.Anchor;
+import com.google.ar.core.Pose;
+import com.google.ar.core.Session;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
@@ -30,13 +37,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     //state of app
     private String appState;
+    //used to format the date
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     //used for Firebase
     private DatabaseReference mDatabase;
     //arraylist of blue light phones
@@ -47,12 +59,22 @@ public class MainActivity extends AppCompatActivity {
     private double currentLat = -1;
     private double currentLong = -1;
     private double bearing = 0;
+    //variable contains the angle in which the image needs to rotate
+    private float turningAngle = 0;
 
     //closest Blue Light Phone
     private BlueLight closestBlueLight = null;
 
     //ARCore
     private ArFragment arFragment;
+    private ModelRenderable arrowRenderable;
+
+    //Compass
+    private float[] mGravity= new float[3];
+    private float[] mGeomagnetic = new float[3];
+    private float azimuth=0f;
+    private float currentAzimuth = 0f;
+    private SensorManager mSensorManager;
 
     private TextView mTextMessage;
 
@@ -75,8 +97,9 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_search:
                     appState = "SEARCH";
                     mTextMessage.setText(R.string.title_search);
-                    checkClosestBlueLight();
+                    //checkClosestBlueLight();
                     showCloestBlueLight();
+                    addAR();
                     return true;
             }
             return false;
@@ -96,6 +119,9 @@ public class MainActivity extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
         //Get firebase database reference
         mDatabase = FirebaseDatabase.getInstance().getReference("blue light phones");
+
+        mSensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+
         firebaseData_init();
         geolocate_init();
         arCore_init();
@@ -106,12 +132,26 @@ public class MainActivity extends AppCompatActivity {
 
     private void arCore_init(){
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+
+        ModelRenderable.builder()
+                // To load as an asset from the 'assets' folder ('src/main/assets/andy.sfb'):
+                .setSource(this, Uri.parse("model.sfb"))
+                .build()
+                .thenAccept(renderable -> arrowRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            builder.setMessage(throwable.getMessage())
+                                    .show();
+                            return null;
+                        });
+
         arFragment.setOnTapArPlaneListener(((hitResult, plane, motionEvent) -> {
             if(appState.compareTo("SEARCH") == 0) {
                 Anchor anchor = hitResult.createAnchor();
 
                 ModelRenderable.builder()
-                        .setSource(this, Uri.parse("arrow.sfb"))
+                        .setSource(this, Uri.parse("model.sfb"))
                         .build()
                         .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
                         .exceptionally(throwable -> {
@@ -129,15 +169,29 @@ public class MainActivity extends AppCompatActivity {
         AnchorNode anchorNode = new AnchorNode(anchor);
         TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
         //rotate the arrow about the z axis
-        //transformableNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 0, 1f), 90f));
+        //transformableNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0), 235f));
+        transformableNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 1f, 0), turningAngle));
         //rotate the arrow about the x axis
-        transformableNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0, 0, 1f), 90f));
+        //transformableNode.setLocalRotation(Quaternion.axisAngle(new Vector3(1f, 1f, 0), 90f));
         transformableNode.setParent(anchorNode);
+        //transformableNode.setRenderable(modelRenderable);
         transformableNode.setRenderable(modelRenderable);
         arFragment.getArSceneView().getScene().addChild(anchorNode);
         transformableNode.select();
     }
 
+    private void addAR(){
+        //Add an Anchor and a renderable in front of the camera
+        Session session = arFragment.getArSceneView().getSession();
+        float[] pos = { 0,0,-1 };
+        float[] rotation = {0,0,0,1};
+        Anchor anchor =  session.createAnchor(new Pose(pos, rotation));
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setRenderable(arrowRenderable);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        ///anchorNode.setParent(arFragment.getArSceneView().getScene());
+        System.out.println("\n###addAR finish run###\n");
+    }
 
     public void firebaseData_init(){
         ValueEventListener dataListener = new ValueEventListener() {
@@ -189,8 +243,9 @@ public class MainActivity extends AppCompatActivity {
                 currentLat = location.getLatitude();
                 currentLong = location.getLongitude();
                 String message = "currentLat: " + currentLat + "\ncurrentLong: " + currentLong;
-                showMessage("HOME",message);
-                //checkClosestBlueLight();
+                //showMessage("HOME",message);
+                checkClosestBlueLight();
+                showLocation();
 
             }
 
@@ -216,7 +271,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showLocation(){
-        String message = "currentLat: " + currentLat + "\ncurrentLong: " + currentLong;
+        String message = "Updated on: " + dateFormat.format(new Date()) +
+                "\ncurrentLat: " + currentLat +
+                "\ncurrentLong: " + currentLong +
+                "\ncurrentAzimuth: " + azimuth;
         showMessage("HOME",message);
     }
 
@@ -237,17 +295,31 @@ public class MainActivity extends AppCompatActivity {
         Collections.sort(blueLights);
         //get the first element in sorted list
         closestBlueLight = blueLights.get(0);
+
         //get the angle between the closest blue light and current position
         bearing = angleFromCoordinate(currentLat,currentLong,closestBlueLight.getLat(),closestBlueLight.getLong());
+        //calculate the angle to rotate the image
+        updateTurningAngle();
         showCloestBlueLight();
+
+        //add new anchor to the scene
+        //addAR();
+    }
+
+    private void updateTurningAngle(){//calculate the angle to rotate the image
+        turningAngle = (float) -((bearing - (azimuth + 235)%360 )%360);
     }
 
     private void showCloestBlueLight(){
         String message;
         if(closestBlueLight != null) {
-            message = "Name: " + closestBlueLight.getName() + "\nLat: " + closestBlueLight.getLat() +
-                    "\nLong: " + closestBlueLight.getLong() + "\nDistance: " + closestBlueLight.getDistance() +
-                    "\nBearing: " + bearing;
+            message = "Name: " + closestBlueLight.getName() +
+                    "\nLat: " + closestBlueLight.getLat() +
+                    "\nLong: " + closestBlueLight.getLong() +
+                    "\nDistance: " + closestBlueLight.getDistance() +
+                    "\nBearing: " + bearing +
+                    "\nHeading: " + azimuth +
+                    "\nTurning Degrees: " + turningAngle;
         }
         else{
             message = "no info available yet";
@@ -272,7 +344,7 @@ public class MainActivity extends AppCompatActivity {
         brng = (brng + 360) % 360;
         //brng = 360 - brng; // count degrees counter-clockwise - remove to make clockwise
 
-        System.out.println("***bearing: " + brng);
+        //System.out.println("***bearing: " + brng);
         return brng;
     }
 
@@ -282,4 +354,60 @@ public class MainActivity extends AppCompatActivity {
             System.out.println(message);
         }
     }
+
+    //stuff for the compass/azimuth
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        final float alpha = 0.97f;
+        synchronized (this) {
+            if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                mGravity[0] = alpha * mGravity[0] + (1 - alpha) * sensorEvent.values[0];
+                mGravity[1] = alpha * mGravity[1] + (1 - alpha) * sensorEvent.values[1];
+                mGravity[2] = alpha * mGravity[2] + (1 - alpha) * sensorEvent.values[2];
+
+            }
+            if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha) * sensorEvent.values[0];
+                mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha) * sensorEvent.values[1];
+                mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha) * sensorEvent.values[2];
+            }
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                azimuth = (float) Math.toDegrees(orientation[0]);
+                azimuth = (azimuth + 360) % 360;
+                //azimuth = azimuth % 360;
+            }
+        }
+        if(Math.abs(currentAzimuth - azimuth) > 2)
+        {
+            currentAzimuth = azimuth;
+            updateTurningAngle();
+            showCloestBlueLight();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
 }
